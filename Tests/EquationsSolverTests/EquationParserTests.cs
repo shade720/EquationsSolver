a6115fc7c0825ddb2c50f5f22949f0a952a273a7
Евгений Михайлюков <mihaylyukov99@gmail.com>
2023-11-22 20:07:29 +0600

    Добавил тесты парсера уравнений

---
+++ Tests/EquationsSolverTests/EquationParserTests.cs
@@ -0,0 +1,60 @@
+using EquationsSolver.Exceptions;
+using EquationsSolver.Models.Readers;
+
+namespace EquationsSolverTests;
+
+public class EquationParserTests
+{
+    private readonly EquationParser _sut = new();
+
+    [Fact]
+    public void Parse_ThreeCoefficients_Success()
+    {
+        // Arrange
+        const int coefficient1 = 1;
+        const int coefficient2 = 0;
+        const int coefficient3 = 2;
+        var coefficientsLine = $"{coefficient1} {coefficient2} {coefficient3}";
+        const int expectedEquationCoefficientsNumber = 3;
+
+        // Act
+        var actualEquation = _sut.Parse(coefficientsLine);
+
+        // Assert
+        Assert.Equal(expectedEquationCoefficientsNumber, actualEquation.Coefficients.Count);
+        Assert.Equal(coefficient1, actualEquation.Coefficients.ElementAt(0));
+        Assert.Equal(coefficient2, actualEquation.Coefficients.ElementAt(1));
+        Assert.Equal(coefficient3, actualEquation.Coefficients.ElementAt(2));
+    }
+
+    [Fact]
+    public void Parse_EmptyLine_ThrowArgumentNullException()
+    {
+        // Arrange
+        var coefficientsLine = string.Empty;
+
+        // Act, Assert
+        Assert.Throws<ArgumentNullException>(() => _sut.Parse(coefficientsLine));
+    }
+
+    [Fact]
+    public void Parse_BadCoefficient_ThrowEquationParseException()
+    {
+        // Arrange
+        const string badCoefficient = "fd";
+        const string coefficientsLine = $"1 {badCoefficient} 2";
+
+        // Act, Assert
+        Assert.Throws<EquationParseException>(() => _sut.Parse(coefficientsLine));
+    }
+
+    [Fact]
+    public void Parse_OneCoefficient_ThrowArgumentException()
+    {
+        // Arrange
+        const string coefficientsLine = "1";
+
+        // Act, Assert
+        Assert.Throws<ArgumentException>(() => _sut.Parse(coefficientsLine));
+    }
+}
